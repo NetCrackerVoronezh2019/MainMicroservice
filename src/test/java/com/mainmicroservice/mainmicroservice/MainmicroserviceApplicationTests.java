f0ea362b1b97814a066a23cc8d1ee9f5a9893682
Armen <armentovmasyan02@gmail.com>
2019-12-13 21:41:55 +0300

    Создал Проект MainMicroservice

---
+++ src/test/java/com/mainmicroservice/mainmicroservice/MainmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainmicroservice.mainmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
