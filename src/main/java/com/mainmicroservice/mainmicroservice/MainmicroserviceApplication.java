0b3a2dcb1d31307f979a5b3f72df7159f70a9876
Armen <armentovmasyan02@gmail.com>
2020-01-17 18:57:23 +0300

    добавил ConsumerThrea

--- src/main/java/com/mainmicroservice/mainmicroservice/MainmicroserviceApplication.java
+++ src/main/java/com/mainmicroservice/mainmicroservice/MainmicroserviceApplication.java
@@ -1,16 +1,21 @@
 package com.mainmicroservice.mainmicroservice;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.mainmicroservice.mainmicroservice.Kafka.KafkaCons;
+import com.mainmicroservice.mainmicroservice.Kafka.ConsumerThread; 
 
 @SpringBootApplication
 public class MainmicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainmicroserviceApplication.class, args);
-		KafkaCons.getPortModel("ports_topic", "mainMicroGroup");
+		
+		CountDownLatch latch=new CountDownLatch(1);
+	    Thread consumerThread =new Thread(new ConsumerThread(latch,"ports_topic","mainGroupX"));
+	    consumerThread.start();
 	}
 
 }
